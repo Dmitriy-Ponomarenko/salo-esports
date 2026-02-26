4853516b3b286e5f71e7d702ae9a906eb2d67154
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2026-02-26 19:01:02 +0100

    eslint update

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// eslint.config.js
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
+
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react-refresh/recommended',
+      'plugin:prettier/recommended',
     ],
-    languageOptions: {
+
+    parser: '@typescript-eslint/parser',
+
+    parserOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+
+    plugins: [
+      '@typescript-eslint',
+      'react',
+      'react-hooks',
+      'react-refresh',
+      'prettier',
+    ],
+
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-])
+]);
